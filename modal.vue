93a045e10a045c33549836f8d94c9641924ebcbe
Brandi Quintos <brandiquintos@gmail.com>
2020-05-07 18:33:54 -0500

    Added new modal code

--- modal.vue
+++ modal.vue
@@ -2,109 +2,102 @@
 /**
  * @fileoverview Modal component
  *
+ * @param {string} size - Size of the modal. small | medium | large
  * @param {string} heading - Heading for the modal
- * @param {string} description - Paragraph text for the modal
- * @param {boolean} close_button - Should there be a close button for the user to 'X' out of the modal? Defaults to false
- * @param {boolean} background_close - Should the user be able to click the background to close the modal? Defaults to false
- * @param {string} image - Image to show in the modal
- * @param {string} icon - Icon to show in the modal
- * @param {string} confirm_text - Text to show in the confirm button
- * @param {string} decline_text - Text to show in the decline button
- * @param {string} checkbox_text - Text to show by the checkbox
- * @param {boolean} checkbox_required - Is the checkbox required to confirm this modal? Defaults to false
+ * @param {string} icon - Icon to show in the button - will use icomoon icons
+ * @param {string} submit_text - Text to show in the submit button
+ * @param {string} cancel_text - Text to show in the cancel button
+ * @param {boolean} complete - Check to see if all required fields are complete before enabling the submit button
  */
 
 export default {
 	props: {
+		size: {
+			type: String,
+			default: '',
+		},
 		heading: {
-			type: String
-        },
-        description: {
-            type: String
-        },
-        close_button: {
+			type: String,
+			default: '',
+		},
+		icon: {
+			type: String,
+			default: '',
+		},
+		submit_text: {
+			type: String,
+			default: '',
+		},
+		cancel_text: {
+			type: String,
+			default: 'Cancel',
+		},
+		complete: {
 			type: Boolean,
-			default: false
-        },
-        background_close: {
-            type: Boolean,
-			default: false
-        },
-        image: {
-			type: String
-        },
-        icon: {
-			type: String
-        },
-        confirm_text: {
-            type: String
-        },
-        decline_text: {
-            type: String
-        },
-        checkbox_text: {
-            type: String
-        },
-        checkbox_required: {
-            type: Boolean,
-            default: false
-        }
+			default: false,
+		}
 	},
-    methods: {
-       showModal() {
-            this.visible = true;
-            this.$emit('show');
-        },
-        closeModal() {
-            this.visible = false;
-            this.$emit('close');
-        },
-        confirm() {
-            this.closeModal();
-            this.$emit('confirm', {checkbox: true});
-        },
-        decline() {
-            this.closeModal();
-            this.$emit('decline');
-        },
-        background_click() {
-            if(this.background_close == true) {
-                this.closeModal();
-            }
-        }
-    },
-    data() {
-        return {
-            visible: false,
-            check: false
-        }
-    },
+	data() {
+		return {
+			visible: false,
+		};
+	},
+	methods: {
+		showModal() {
+			this.visible = true;
+			this.$emit('show');
+		},
+		hideModal() {
+			this.visible = false;
+			this.$emit('hide');
+		},
+		submit() {
+			this.hideModal();
+			this.$emit('submit');
+		},
+		cancel() {
+			this.hideModal();
+			this.$emit('cancel');
+		},
+	},
+
 };
 </script>
 
 
 <template>
-  <div v-if="visible" class="modal-overlay" @click.self="background_click">
-    <div class="modal-container">
-        <div v-if="close_button == true" class="close" @click="closeModal">
-            <i class="icon-times"></i>
-        </div>
-        <div v-if="image" class="mb20">
-            <img :src='image' />
-        </div>
-        <div class="icon mb20" v-if="icon">
-            <i :class='icon' />
-        </div>
-        <h1 class="mb20">{{heading}}</h1>
-        <p>{{description}}</p>
-        <label v-if="checkbox_text" class="radio-check mt10">
-            <input type="checkbox" v-model="check" name="check"/> {{checkbox_text}}
-        </label>
-        <div v-if="decline_text || confirm_text" class="buttons mt20">
-            <button v-if="decline_text" class="btn btn-primary btn-primary--gray" @click="decline">{{decline_text}}</button>
-            <button v-if="confirm_text && !checkbox_required" class="btn btn-primary" @click="confirm">{{confirm_text}} </button>
-            <button v-if="confirm_text && checkbox_required" class="btn btn-primary" @click="confirm" :class="{disabled: !check}">{{confirm_text}}</button>
-        </div>
-    </div>
-  </div>
+	<div v-if="visible" class="modal-overlay">
+		<div class="modal-container" :class="size">
+			<div class="modal-header">
+				<button
+					type="button"
+					@click="hideModal"
+					class="close"
+				>
+					<i class="icon-times" />
+				</button>
+				<h1 class="modal-title">{{ heading }}</h1>
+			</div>
+			<div class="modal-body">
+				<slot name="modal-content" />
+			</div>
+			<div class="modal-footer">
+				<slot name="extra-button" />
+				<button
+					class="btn btn-transparent"
+					@click="cancel"
+				>
+					{{ cancel_text }}
+				</button>
+				<button
+					class="btn btn-primary"
+					:class="{'disabled': !complete }"
+					@click="submit"
+				>
+					<i v-if="icon" :class="icon" />
+					<span class="btn-text">{{ submit_text }}</span>
+				</button>
+			</div>
+		</div>
+	</div>
 </template>
